78dc1ecabb57abee3aca98e5205d4aa79d1093cc
siga44 <seregqaaa@yandex.ru>
2020-07-28 00:26:44 +0500

    Layout changes, added main container

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 const wrapper = document.querySelector('.wrapper');
+const main = wrapper.querySelector('.main__body');
 const navbar = wrapper.querySelector('.navbar');
 const burger = navbar.querySelector('#burger');
 const menu = navbar.querySelector('#menu');
 
 navbar.addEventListener('click', event => {
   const target = event.target;
+  const menuItems = [...menu.children].map(li => li.lastElementChild);
+  const elementWithActive = [...wrapper.children, ...burger.children, ...menuItems, main];
   if (target.hasAttribute('data-burger-button')) {
-    const menuItems = [...menu.children].map(li => li.lastElementChild);
-    [navbar, ...burger.children, ...menuItems].forEach(item => item.classList.toggle('active'));
+    elementWithActive.forEach(item => item.classList.toggle('active'));
   }
 });
