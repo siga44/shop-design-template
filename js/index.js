36607ee536825ccd894cced57414f66f5286fc68
siga44 <seregqaaa@yandex.ru>
2020-07-27 15:58:25 +0500

    Fixed styles, added script

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-const navbar = document.querySelector('.navbar');
+const wrapper = document.querySelector('.wrapper');
+const navbar = wrapper.querySelector('.navbar');
 const burger = navbar.querySelector('#burger');
 const menu = navbar.querySelector('#menu');
-const menuItems = navbar.querySelectorAll('.menu__item-full');
 
 navbar.addEventListener('click', event => {
   const target = event.target;
   if (target.hasAttribute('data-burger-button')) {
-    navbar.classList.toggle('active');
-    menuItems.forEach(item => item.classList.toggle('active'));
+    const menuItems = [...menu.children].map(li => li.lastElementChild);
+    [navbar, ...burger.children, ...menuItems].forEach(item => item.classList.toggle('active'));
   }
 });
